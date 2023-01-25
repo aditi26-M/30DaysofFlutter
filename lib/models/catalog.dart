8f6276a976e24fb316d8722d0097f9bb08bed2b7
Aditi <aditimaheshwari3001@gmail.com>
2023-01-25 15:28:05 +0530

    Day 12 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,17 @@
-class Item{
-  final String id;
+class CatalogModel {
+  static final items = [
+    Item(
+        1,
+        "Poco M3 pro 5G",
+        "The POCO M3 is a stylish personal device that offers immersive visuals, seamless performance, and long battery life for uninterrupted functioning.",
+        10000,
+        "blue",
+        "https://www.kibotek.com/wp-content/uploads/2021/05/kiboTEK_poco_m3_pro_006.jpg")
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,9 +20,3 @@ class Item{
 
   Item(this.id, this.name, this.desc, this.price, this.color, this.image);
 }
-
-final Products = [
-  
-    Item("Code", "Poco", "dfsgsrg", 10000, "blue", "vhgg")
-  
-];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item.dart';
 
 class HomePage extends StatelessWidget {
   String name = "Flutter";
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Store"),
       ),
-      body: Container(
-        height: 100,
-        width: 100,
-        color: Colors.lightBlueAccent,
-        
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(7, 25, 0, 0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: ((context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          }),
+        ),
       ),
-      
       drawer: MyDrawer(),
     );
   }
